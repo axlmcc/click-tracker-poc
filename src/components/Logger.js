2252137c31bfb1825605e8b74facbf0f272ce504
Axl McCracken <axl@ucidigital.com>
2019-04-25 08:57:27 -0500

    Add Event Listeners and sane defaults

--- src/components/Logger.js
+++ src/components/Logger.js
@@ -1,8 +1,31 @@
 class Logger {
-  constructor(gather_list = ['timestamp', 'page_url', 'href']) {
+  constructor(options) {
+    const {
+      monitor_selector = '.track-clicks',
+      gather_list = [
+        'timestamp',
+        'platform',
+        'page_url',
+        'referrer',
+        'href',
+        'contents'
+      ]
+    } = options || {};
+    this.monitor_selector = monitor_selector;
     this.gather_list = gather_list;
+    this.addEventListeners();
   }
-  print(data) {
-    console.log('tests');
+  addEventListeners() {
+    let watchList = document.querySelectorAll(this.monitor_selector);
+    watchList.forEach(element => {
+      element.addEventListener('click', event => {
+        event.preventDefault();
+        // this.log_data(element, this.gather_list);
+        window.location.href = element.href;
+      });
+    });
+  }
+  print() {
+
   }
 }
